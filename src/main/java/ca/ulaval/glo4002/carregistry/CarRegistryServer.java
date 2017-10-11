b68a83c30f3adda9757afd7544bb51fe28370635
vincent bg <vincent.boily-grant.1@ulaval.ca>
2017-10-11 13:32:13 -0400

    df

--- src/main/java/ca/ulaval/glo4002/carregistry/CarRegistryServer.java
+++ src/main/java/ca/ulaval/glo4002/carregistry/CarRegistryServer.java
@@ -1,5 +1,9 @@
 package ca.ulaval.glo4002.carregistry;
 
+import java.util.EnumSet;
+
+import javax.servlet.DispatcherType;
+
 import org.eclipse.jetty.server.Server;
 import org.eclipse.jetty.servlet.ServletContextHandler;
 import org.eclipse.jetty.servlet.ServletHolder;
@@ -9,42 +13,46 @@ import org.glassfish.jersey.servlet.ServletContainer;
 import ca.ulaval.glo4002.carregistry.domain.CarOwner;
 import ca.ulaval.glo4002.carregistry.domain.CarRegistry;
 import ca.ulaval.glo4002.carregistry.persistence.InMemoryCarRegistry;
+import ca.ulaval.glo4002.carregistry.rest.filters.EntityManagerContextFilter;
 
 public class CarRegistryServer {
-	  public static void main(String[] args) {
-	        new CarRegistryServer().run();
-	    }
+	public static void main(String[] args) {
+		new CarRegistryServer().run();
+	}
 
-	    public void run() {
-	    	prefillDatabase();
-	        startServer();
-	    }
+	public void run() {
+		prefillDatabase();
+		startServer();
+	}
 
-		private void prefillDatabase() {
-			CarRegistry carRegistry = new InMemoryCarRegistry();
-			carRegistry.insert(new CarOwner("John Doe"));
-			carRegistry.insert(new CarOwner("Jane Doe"));
+	private void prefillDatabase() {
+		CarRegistry carRegistry = new InMemoryCarRegistry();
+		carRegistry.insert(new CarOwner("John Doe"));
+		carRegistry.insert(new CarOwner("Jane Doe"));
+	}
+
+	private void startServer() {
+		int httpPort = 9595;
+
+		Server server = new Server(httpPort);
+		ServletContextHandler servletContextHandler = new ServletContextHandler(server, "/");
+		configurerJersey(servletContextHandler);
+		try {
+			server.start();
+			server.join();
+		} catch (Exception e) {
+			e.printStackTrace(); // Une des rare fois qu'on peut!
+		} finally {
+			server.destroy();
 		}
+	}
 
-		private void startServer() {
-			int httpPort = 9595;
+	private void configurerJersey(ServletContextHandler servletContextHandler) {
+		ServletContainer container = new ServletContainer(
+				new ResourceConfig().packages("ca.ulaval.glo4002.carregistry.rest"));
+		ServletHolder jerseyServletHolder = new ServletHolder(container);
+		servletContextHandler.addServlet(jerseyServletHolder, "/*");
+		servletContextHandler.addFilter(EntityManagerContextFilter.class, "/*", EnumSet.of(DispatcherType.REQUEST));
 
-	        Server server = new Server(httpPort);
-	        ServletContextHandler servletContextHandler = new ServletContextHandler(server, "/");
-	        configurerJersey(servletContextHandler);
-			try {
-				server.start();
-				server.join();
-			} catch (Exception e) {
-				e.printStackTrace(); // Une des rare fois qu'on peut!
-			} finally {
-				server.destroy();
-			}
-		}
-
-	    private void configurerJersey(ServletContextHandler servletContextHandler) {
-	        ServletContainer container = new ServletContainer(new ResourceConfig().packages("ca.ulaval.glo4002.carregistry.rest"));
-	        ServletHolder jerseyServletHolder = new ServletHolder(container);
-	        servletContextHandler.addServlet(jerseyServletHolder, "/*");
-	    }
+	}
 }
---
+++ src/main/java/ca/ulaval/glo4002/carregistry/persistence/HibernateCarRegistry.java
@@ -0,0 +1,49 @@
+package ca.ulaval.glo4002.carregistry.persistence;
+
+import java.util.Collection;
+
+import javax.persistence.EntityManager;
+
+import ca.ulaval.glo4002.carregistry.domain.CarOwner;
+import ca.ulaval.glo4002.carregistry.domain.CarRegistry;
+
+public class HibernateCarRegistry implements CarRegistry {
+	private EntityManager entityManager;
+
+	public HibernateCarRegistry() {
+		this.setEntityManager(new EntityManagerProvider().getEntityManager());
+	}
+
+	@Override
+	public CarOwner findOwner(int ownerId) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void insert(CarOwner owner) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void update(CarOwner owner) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public Collection<CarOwner> findAllOwners() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public EntityManager getEntityManager() {
+		return entityManager;
+	}
+
+	public void setEntityManager(EntityManager entityManager) {
+		this.entityManager = entityManager;
+	}
+
+}
